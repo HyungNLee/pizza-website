2367f0cc31cc979f54fb44a6ff66a345438a4690
Hyung Lee <hyungnaelee@gmail.com>
2018-08-24 12:25:21 -0700

    Add divs inside modal. Add radio buttons for size options.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,24 @@
+//Back-end
+//Pizza Constructor
+function Pizza() {
+  this.size = [];
+  this.toppings = [];
+  this.sides = [];
+  this.orderNumber = "";
+  this.customerName = "";
+}
+
+//Function to calculate pizza cost
+Pizza.prototype.calculateCost = function() {
+  //add formula to calculate cost of pizza
+}
+
+Pizza.prototype.confirmationNumber = function() {
+  //add formula to randomly generate a number.
+}
+
+//User Interface
 $(document).ready(function() {
-  $("#pizzaModalButton").click(function() {
-    alert("Hi");
-    $("#pizzaOrderModal").show();
-  })
+
 
 })
