f508008a999f203da0a89957c9724bc925810e73
Hyung Lee <hyungnaelee@gmail.com>
2018-08-24 12:43:27 -0700

    Add funtionality to buttons in model to switch between size, toppings and side menus.

--- js/scripts.js
+++ js/scripts.js
@@ -20,5 +20,23 @@ Pizza.prototype.confirmationNumber = function() {
 //User Interface
 $(document).ready(function() {
 
+  //Click function to show size menu and hide others
+  $("#sizeMenuButton").click(function(){
+    $(".toppings-page").hide();
+    $(".sides-page").hide();
+    $(".size-page").show();
+  })
+  //Click function to show toppings menu and hide others
+  $("#toppingMenuButton").click(function(){
+    $(".toppings-page").show();
+    $(".sides-page").hide();
+    $(".size-page").hide();
+  })
+  //Click function to show sides menu and hide others
+  $("#sidesMenuButton").click(function(){
+    $(".toppings-page").hide();
+    $(".sides-page").show();
+    $(".size-page").hide();
+  })
 
 })
